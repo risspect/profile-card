c7d64ae4a917617cdff8a61c93dba2cc0e90d4f6
Risspect <risnafadillah08@gmail.com>
2021-06-12 21:08:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,119 +1,27 @@
-document.getElementById('risna').addEventListener('click', function() {
-	Swal.fire({
-		title: 'Halo Sayang',
-		text: 'Aku ada pertanyaan nih buat kamu',
-		icon: 'question',
-		confirmButtonText: 'Lanjut..'
-	}).then(function () {
-		Swal.fire({
-			title: 'Tapi Jawab nya jujur yaa..',
-			text: 'Awas aja boong!!',
-			icon: 'warning',
-			confirmButtonText: 'Iyaa'
-		}).then(function () {
-			Swal.fire({
-				title: 'Inget, Jangan boong!!',
-				text: 'Okee?',
-				icon: 'error',
-				confirmButtonText: 'Okee'
-			}).then(function () {
-				const { value: name } = Swal.fire({
-					title: 'Sebelumnya..',
-					inputLabel: 'Kasih tau dulu dong nama kamu..',
-					input: 'text',
-					confirmButtonText: 'udahh',
-					inputValidator: (value) => {
-						if (!value) {
-							return('kasih tau dong..')
-						} else {
-							nama = value;
-						}
-					}
-				}).then((result) => {
-					Swal.fire({
-						title: `Oke ${nama}`,
-						text: 'Sekarang aku mulai pertanyaannya yaa..',
-						icon: 'success',
-						confirmButtonText: 'Oke..'
-					}).then(function () {
-						Swal.fire({
-							title: 'Emm..',
-							text: `${nama} sayang ga sama Risna?`,
-							icon: 'question',
-							showDenyButton: true,
-							confirmButtonText: `Sayang:)`,
-							denyButtonText: `Gak!:(`
-						}).then((result) => {
-							if (result.isConfirmed) {
-								Swal.fire({
-									title: 'Hehe..',
-									text: `Risna Juga sayang sama ${nama}`,
-									confirmButtonText: 'Iyaa..'
-								}).then(function() {
-									Swal.fire({
-										title: 'Coba..',
-										inputLabel: `Berapa persen sayangnya ${nama} sama Risna?`,
-										input: 'range',
-										inputAttributes: {
-											min: '1',
-											max: '100',
-											step: 1
-										},
-										inputValue: 50
-									}).then((e) => {
-										const val = e.value
-										Swal.fire({
-											title: 'Emm..',
-											text: `Makasih yaa ${nama} udah sayang sama risna ${val}%`,
-											confirmButtonText: 'iyaa..'
-										}).then(function () {
-											Swal.fire({
-												title: 'Sekarang..',
-												text: `${nama} kangen ga sama Risna?`,
-												confirmButtonText: 'Kangen:(',
-												showDenyButton: true,
-												denyButtonText: 'Gak_-'
-											}).then((result) => {
-												if (result.isConfirmed) {
-													Swal.fire({
-														title: 'hehe..',
-														text: `Sama Risna juga kangen sama ${nama}`,
-														confirmButtonText: 'Iya sayangg'														
-													}).then(function () {
-														Swal.fire({
-															title: `Terakhir nih buat ${nama}`,
-															text: 'Coba tulis sesuatu tentang Risna sama tulis kapan kita mau ketemu, klik tanda hati di bawah yaa',
-															footer: '<a href="https://secreto.site/id/20364229"><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i></a>',
-															showConfirmButton: false,
-															icon: 'success'
-														})
-													})
-												} else {
-													Swal.fire({
-														title: 'OKE DEH',
-														text: 'Bye!',
-														showConfirmButton: false,
-														showCancelButton: true,
-														cancelButtonText: 'Ok'
-													})
-												}
-											})
-										})
-									})
-								})
-							} else {
-								Swal.fire({
-									title: 'Oh gitu',
-									text: `Jadi selama ini ${nama} ga sayang sama aku?`,
-									showConfirmButton: false,
-									showCancelButton: true,
-									cancelButtonText: 'Ya'
-								})
-							}
-						})
-					})
-				})
-			})
-		})
-	})
-})
+;(function () {
+    let card  = document.getElementsByClassName('card')[0],
+        moved = 0,
+        interval;
+
+    if (!card) return;
+    
+    card.addEventListener('click', function (event) {
+        clearInterval(interval);
+        card.style.transform = '';
+        
+        // Do not flip the card if the user is trying to
+        // tap a link.
+        if (event.target.nodeName === 'A') {
+            return;
+        }
+        
+        let cName   = card.getAttribute('data-toggle-class');
+        let toggled = card.classList.contains(cName);
+        card.classList[toggled ? 'remove' : 'add'](cName);
+    });
+    
+    interval = setInterval(function () {
+        moved = moved ? 0 : 10;
+        card.style.transform = 'translateY(' + moved + 'px)';
+    }, 1500);
+})();
